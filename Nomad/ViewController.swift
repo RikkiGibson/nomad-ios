f4cf6cb713667c73e36170f1e8b58985b89a18c8
Rikki Gibson <rikkigibson@gmail.com>
2017-09-30 17:56:58 -0700

    Experiment with local NetService

--- Nomad/ViewController.swift
+++ Nomad/ViewController.swift
@@ -1,25 +1,57 @@
-//
-//  ViewController.swift
-//  Nomad
-//
-//  Created by Rikki Gibson on 9/30/17.
-//  Copyright © 2017 Rikki Gibson. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+let DOMAIN = "local"
+let TYPE = "_nomad._tcp."
+let SERVICE_NAME = "nomad"
+let SERVICE_PORT: Int32 = 6543
+
+class ViewController: UIViewController, NetServiceDelegate {
+    
+    let netService = NetService(
+        domain: DOMAIN,
+        type: TYPE,
+        name: SERVICE_NAME,
+        port: SERVICE_PORT)
+    
+    let browser = NetServiceBrowser()
+    let delegate = NomadNetBrowserDelegate()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        netService.delegate = self
+        netService.publish()
+        
+        browser.delegate = delegate
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // - MARK: NetServiceDelegate
+    func netServiceDidPublish(_ sender: NetService) {
+        DispatchQueue.main.async {
+            self.browser.searchForServices(ofType: TYPE, inDomain: "")
+        }
+    }
 }
 
+class NomadNetBrowserDelegate: NSObject, NetServiceBrowserDelegate {
+    func netServiceBrowserWillSearch(_ browser: NetServiceBrowser) {
+        print("Will search")
+    }
+    
+    func netServiceBrowserDidStopSearch(_ browser: NetServiceBrowser) {
+        print("Stopped search")
+    }
+    
+    func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
+        print(service)
+    }
+    
+    func netServiceBrowser(_ browser: NetServiceBrowser, didFindDomain domainString: String, moreComing: Bool) {
+        print(domainString)
+    }
+}
