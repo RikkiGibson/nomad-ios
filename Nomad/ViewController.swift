5cb4e92029598d9e2d26574f32f8bfe702eb1e8f
Rikki Gibson <rikkigibson@gmail.com>
2017-09-30 17:08:04 -0700

    Initial Commit

---
+++ Nomad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nomad
+//
+//  Created by Rikki Gibson on 9/30/17.
+//  Copyright © 2017 Rikki Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
